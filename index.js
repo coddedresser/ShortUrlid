66a43a73bdc92694d7d7106fdd8139aee242fa81
Ayush Tripathi <ayushtripathi.2104@gmail.com>
2025-07-05 21:54:12 +0530

    Final Commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const staticRoute=require('./routers/staticRouter.js');
 const PORT=3000;
 
 //MongoDB Connect
-const MongoUrl="mongodb+srv://jackalxa1:RnWaxvaSUOVj2m6I@cluster0.op82zh4.mongodb.net/";
+const MongoUrl="";
 connectToMongoDB(MongoUrl).then(()=>console.log('mongoDB Connected'));
 
 //Middlewares
